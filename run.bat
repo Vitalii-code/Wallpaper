008945626b83de5952b5c225f6cb6973a4bcc2a4
Vitalya <vitalikkmita@gmail.com>
2022-09-08 20:05:20 +0300

    some folders sort and fixes

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
+echo on 
+echo "[Wallpaper] Loading..."
+
 python -m venv venv
-venv\Scripts\pip.exe install -r windows_requirements.txt
-start venv\Scripts\pythonw.exe main.pyw
+venv\Scripts\pip.exe install -r requirements\windows_requirements.txt
+start venv\Scripts\pythonw.exe src\main.pyw
 
 
--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+echo -e "[Wallpaper] Loading..."
 python3 -m venv venv
-venv/bin/pip3 install -r linux_requirements.txt
-echo -e "\n The program must be in tray"
-venv/bin/python3 main.pyw
+venv/bin/pip3 install -r requirements/linux_requirements.txt
+echo -e "\n[Wallpaper] The program must be in tray"
+venv/bin/python3 src/main.pyw &
 
 
