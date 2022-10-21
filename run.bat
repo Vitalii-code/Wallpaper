b0409ad6aa52d9124330cb568b1e24af292d727b
Vitalya <vitalikkmita@gmail.com>
2022-10-21 11:14:03 +0300

    add "start" to download script

--- run.bat
+++ run.bat
@@ -7,7 +7,7 @@ if not exist "python\" (
 	echo [Wallpaper] Python is not installed
 	
 	echo [Wallpaper] Python is installing...
-	downloader\aria2c.exe "https://www.python.org/ftp/python/3.9.13/python-3.9.13-embed-amd64.zip" -o "python.zip"
+	start /WAIT /b downloader\aria2c.exe "https://www.python.org/ftp/python/3.9.13/python-3.9.13-embed-amd64.zip" -o "python.zip" < NUL
 	
 	md python
 	tar -xf python.zip -C python
@@ -18,7 +18,7 @@ if not exist "python\" (
 		echo import site
 	)>>"python\python39._pth"
 
-    downloader\aria2c.exe "https://bootstrap.pypa.io/get-pip.py" -o "python\get-pip.py"
+    start /WAIT /b downloader\aria2c.exe "https://bootstrap.pypa.io/get-pip.py" -o "python\get-pip.py" < NUL
 	python\python.exe python\get-pip.py
 
 	echo [Wallpaper] Python is installed
