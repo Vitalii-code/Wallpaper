4dc2d2bfe0f29b5674ab67a8730a0ef345dfb363
Vitalya <vitalikkmita@gmail.com>
2022-10-29 12:55:38 +0300

    fix Linux sh file

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 
 echo -e "[Wallpaper] Loading..."
-python3 -m venv venv
-venv/bin/pip3 install -r requirements/linux_requirements.txt
+python3 -m ensurepip --upgrade # install pip
+python3 -m pip install virtualenv # install venv
+python3 -m venv venv # creating venv
+venv/bin/pip3 install -r requirements/linux_requirements.txt # installing requirements
 echo -e "\n[Wallpaper] The program must be in tray"
 venv/bin/python3 src/main.pyw
