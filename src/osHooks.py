974db560c8767a1028c57e228120e9fee4445286
Vitalya <vitalikkmita@gmail.com>
2023-06-05 18:07:16 +0100

    formatting

--- src/osHooks.py
+++ src/osHooks.py
@@ -33,37 +33,49 @@ def set_wallpaper(path: str):
         ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
 
     elif platform.system() == "Linux":
-        if os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-            os.system(f'gsettings set org.gnome.desktop.background picture-uri "{path}"')
-            os.system(f'gsettings set org.gnome.desktop.background picture-uri-dark "{path}"')
+        if os.environ.get("GNOME_DESKTOP_SESSION_ID"):
+            os.system(
+                f'gsettings set org.gnome.desktop.background picture-uri "{path}"'
+            )
+            os.system(
+                f'gsettings set org.gnome.desktop.background picture-uri-dark "{path}"'
+            )
 
-        elif os.environ.get('KDE_FULL_SESSION') == 'true':
+        elif os.environ.get("KDE_FULL_SESSION") == "true":
             command = """dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string: var Desktops = desktops();for (i=0;i<Desktops.length;i++) { d = Desktops[i]; d.wallpaperPlugin = "org.kde.image"; d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General"); d.writeConfig("Image", "%s");}'"""
             os.system(command % path)
 
-
-        elif os.environ.get('DESKTOP_SESSION') == "xfce":
-            os.system(f'xfconf-query -c xfce4-desktop -p  /backdrop/screen0/monitor0/workspace0/last-image -s "{path}"')
+        elif os.environ.get("DESKTOP_SESSION") == "xfce":
+            os.system(
+                f'xfconf-query -c xfce4-desktop -p  /backdrop/screen0/monitor0/workspace0/last-image -s "{path}"'
+            )
 
 
 def get_wallpaper():
     if platform.system() == "Windows":
         ubuf = ctypes.create_unicode_buffer(512)
-        ctypes.windll.user32.SystemParametersInfoW(win32con.SPI_GETDESKWALLPAPER, len(ubuf), ubuf, 0)
+        ctypes.windll.user32.SystemParametersInfoW(
+            win32con.SPI_GETDESKWALLPAPER, len(ubuf), ubuf, 0
+        )
         name = ubuf.value
         name = os.path.splitext(name)[0]
         name = name + ".jpg"
         return name
 
     elif platform.system() == "Linux":
-        if os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-            name = subprocess.check_output(["gsettings get org.gnome.desktop.background picture-uri"], shell=True)
+        if os.environ.get("GNOME_DESKTOP_SESSION_ID"):
+            name = subprocess.check_output(
+                ["gsettings get org.gnome.desktop.background picture-uri"], shell=True
+            )
             return decode_string_from_terminal(name)
 
-
-        elif os.environ.get('KDE_FULL_SESSION') == 'true':
-            name = subprocess.check_output(["""kreadconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group "Containments" --group "1" --group "Wallpaper" --group "org.kde.image" --group "General" --key "Image" """],
-                                           shell=True)
+        elif os.environ.get("KDE_FULL_SESSION") == "true":
+            name = subprocess.check_output(
+                [
+                    """kreadconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group "Containments" --group "1" --group "Wallpaper" --group "org.kde.image" --group "General" --key "Image" """
+                ],
+                shell=True,
+            )
             return decode_string_from_terminal(name)
 
         # xfce - "xfconf-query -c xfce4-desktop -p insert_property_here -s path/image"
@@ -72,11 +84,11 @@ def get_wallpaper():
 
 def decode_string_from_terminal(name):
     try:
-        name = name.decode(encoding='UTF-8')
+        name = name.decode(encoding="UTF-8")
         name = name.replace("%20", " ")
         name = name.replace("'", "")
-        name = name.rstrip('\n')
-        name = name.split('file://')[1]
+        name = name.rstrip("\n")
+        name = name.split("file://")[1]
     except:
         pass
     return name
